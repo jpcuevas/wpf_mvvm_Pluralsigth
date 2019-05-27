ee800dd013d63db282c0aa4bc15db4ea9813f31c
Juan Pablo Cuevas Sclopis <jpcuevas@users.noreply.github.com>
2019-05-27 14:13:52 -0500

    Add files via upload
    Innitial Commit

---
+++ ZzaDashboard/Customers/CustomerListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using ZzaDashboard;
+using ZzaDashboard.Services;
+
+namespace ZzaDashboard.Customers
+{
+    public class CustomerListViewModel
+    {
+        private ICustomersRepository _repo = new CustomersRepository();
+        public CustomerListViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(
+                new System.Windows.DependencyObject())) return;
+            Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result);
+        }
+        
+        public ObservableCollection<Customer> Customers { get; set; }
+    }
+}
